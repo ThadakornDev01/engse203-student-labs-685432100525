67f87197cbcea1ff2f841062eb6052d888521c65
ThadakornDev01 <Thadakon2004@gmail.com>
2026-08-09 18:15:11 +0700

    build: prepare GitHub Pages deployment

--- labs/week-03/source/src/counter.js
+++ labs/week-03/source/src/counter.js
@@ -6,8 +6,4 @@ export function setupCounter(element) {
   }
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0)
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 174a4c6bcbf2a9633a26e8f7919e0d24d57ebe8a
